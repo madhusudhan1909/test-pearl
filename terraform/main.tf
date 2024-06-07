2851f44ef864c3bdf197697996a2affa45f34729
madhu <madhusudhan1909@gmail.com>
2024-06-07 15:13:33 +0530

    first commit

---
+++ terraform/main.tf
@@ -0,0 +1,69 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_ecs_cluster" "example" {
+  name = "example-cluster"
+}
+
+resource "aws_ecs_task_definition" "example" {
+  family                   = "hello-world-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  container_definitions    = jsonencode([{
+    name      = "hello-world-app"
+    image     = "your-dockerhub-username/hello-world-app:latest"
+    cpu       = 256
+    memory    = 512
+    essential = true
+    portMappings = [{
+      containerPort = 3000
+      hostPort      = 3000
+    }]
+  }])
+}
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_ecs_service" "example" {
+  name            = "hello-world-service"
+  cluster         = aws_ecs_cluster.example.id
+  task_definition = aws_ecs_task_definition.example.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = ["subnet-05490c5b9c8deb8c0","subnet-02be771d8ca7bd7da"]
+    security_groups  = ["sg-00d773917a5f77d0c"]
+    assign_public_ip = true
+  }
+}
+
+output "ecs_cluster_name" {
+  value = aws_ecs_cluster.example.name
+}
+
+output "ecs_service_name" {
+  value = aws_ecs_service.example.name
+}
+
