4feecdd51bdffe4515bedc20b68e39895397a112
madhu <madhusudhan1909@gmail.com>
2024-06-08 10:48:14 +0530

    tf new

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,39 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Define the VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# Define subnets
+resource "aws_subnet" "main" {
+  count                   = 2
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+  availability_zone       = element(["us-east-1a", "us-east-1b"], count.index)
+  map_public_ip_on_launch = true
+}
+
+# Define the security group
+resource "aws_security_group" "allow_http" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 # Define the ECS cluster
 resource "aws_ecs_cluster" "example" {
   name = "example-cluster"
@@ -59,9 +92,53 @@ resource "aws_ecs_service" "example" {
   launch_type     = "FARGATE"
 
   network_configuration {
-    subnets          = var.subnets
-    security_groups  = var.security_groups
+    subnets          = [for subnet in aws_subnet.main[*] : subnet.id]
+    security_groups  = [aws_security_group.allow_http.id]
     assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.main.arn
+    container_name   = "hello-world-app"
+    container_port   = 80
+  }
+}
+
+# Define the load balancer
+resource "aws_lb" "main" {
+  name               = "hello-world-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.allow_http.id]
+  subnets            = [for subnet in aws_subnet.main[*] : subnet.id]
+}
+
+# Define the target group
+resource "aws_lb_target_group" "main" {
+  name     = "hello-world-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200-399"
+  }
+}
+
+# Define the listener
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
   }
 }
 
---
+++ terraform/outputs.tf
@@ -0,0 +1,5 @@
+output "load_balancer_dns_name" {
+  description = "The DNS name of the load balancer"
+  value       = aws_lb.main.dns_name
+}
+
